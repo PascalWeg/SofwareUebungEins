a76027e8b4d86a26e78cf7793bf25ef08a1718b8
PascalWeg <167075504+PascalWeg@users.noreply.github.com>
2024-04-16 12:20:17 +0200

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,5 @@
 public class Client extends RomanNumberTransformer {
-    public static void main(String[] args){
-
-       Client c = new Client();
-     c.printTransformation(1008);
-    }
+   
 
     public void printTransformation(int number) {
         String result = "" + super.transformNumber(number); // Hier fehlt noch was
