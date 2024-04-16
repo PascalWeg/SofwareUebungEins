13456c83a2ae218fb7078da87b5556555dc02260
PascalWegn <LonelyFrog@freenet.de>
2024-04-16 12:32:18 +0200

    Heh

--- src/UebungEins/Client.java
+++ src/UebungEins/Client.java
@@ -1,9 +1,6 @@
+package UebungEins;
 public class Client extends RomanNumberTransformer {
-    public static void main(String[] args){
 
-       Client c = new Client();
-     c.printTransformation(1008);
-    }
 
     public void printTransformation(int number) {
         String result = "" + super.transformNumber(number); // Hier fehlt noch was
